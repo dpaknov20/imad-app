75953dd1fcad7d0d9a26d882f5891411527610c0
Deepak Agarwal <adeepak269@gmail.com>
2017-08-18 22:33:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/sample_page', function (req, res) {
+app.get('/home', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sample_page.html'));
 });
 
-app.get('/sample_page/login', function (req, res) {
+app.get('/home/login', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
 });
 
-app.get('/sample_page/signup', function (req, res) {
+app.get('/home/signup', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'signup.html'));
 });
 
