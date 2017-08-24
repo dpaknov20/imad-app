97031681f5a14bd73d5a2872e343a0edbdf2f754
Deepak Agarwal <adeepak269@gmail.com>
2017-08-24 13:48:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
+var check=document.getElementbyID("input");
+check.onClick=function() {
+    var inp=document.getElementbyID("user");
+    var str = "dpaknov20";
+    var n = str1.localeCompare(str2);
+    if(n!=0)
+    document.getElementById("demo").innerHTML = n;
+}
 
-var check=document.getElementbyID("user");
 
