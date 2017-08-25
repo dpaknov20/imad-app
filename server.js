5e5375a6f90e549cb47f78fea3e3bde9c238f3ba
Deepak Agarwal <adeepak269@gmail.com>
2017-08-25 17:59:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool = require('pg').Pool;
 var config = {
     user: 'adeepak269',
     database: 'adeepak269',
-    host: 'db-imad.hasura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     paswword: process.env.DB_PASSWORD
 };
