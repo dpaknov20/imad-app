392cff209a05ce423f54179ba97a6b7261163700
Deepak Agarwal <adeepak269@gmail.com>
2017-08-26 23:48:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,28 @@
-var cli=Document.getElementById("user");
-cli.onclick=function myFunction() 
-{
-            var strk = document.getElementById("user");
-            var str1=strk.value;
-            var str2 = "dpaknov20";
-            var n = str1.localeCompare(str2);
-            if(n!==0)
-            {
-                document.getElementById("user").innerHTML="";
-                alert("please enter a valid username");
-            }
-        };
+var submit=document.getElementById('submit_btn');
+submit.onclick = function() {
+  //create a request
+  var request = new XMLHttpRequest();
+  //capture the response and store it in the variable
+  
+  request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE)  {
+        if(request.status===200)
+        {
+            console.log('user logged in');
+            alert('logged in successfully');
+        }
+        else if(request.status===500)
+        {
+            alert('something went wrong on the server');
+        }
+        else if(request.status===403)
+        {
+            alert('username/password is incorrect');
+        }
+    }
+  };
+  var username = document.getElementById('user').value;
+  var password = document.getElementById('psw').value;
+  request.open('POST','http://adeepak269.imad.hasura-app.io/home/login/submit',true);
+  request.send(JSON.stringify({username: username,password: password}));
+};
