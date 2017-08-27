b4d8d35f47dd600d73668d5924c6b7f10c3919ff
Deepak Agarwal <adeepak269@gmail.com>
2017-08-27 12:04:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,4 +29,36 @@
                   request.open('POST', 'http://adeepak269.imad.hasura-app.io/login', true);
                   request.setRequestHeader('Content-Type','application/json');
                   request.send(JSON.stringify({"username": username, "password": password}));
+};
+ var register=document.getElementById('register_btn');
+                 register.onclick = function() {
+                  //create a request
+                  var request = new XMLHttpRequest();
+                  //capture the response and store it in the variable
+                  
+                  request.onreadystatechange = function() {
+                    if(request.readyState === XMLHttpRequest.DONE)  {
+                        if(request.status === 200)
+                        {
+                            alert('registered successfully');
+                        }
+                        else if(request.status === 500)
+                        {
+                            alert('something went wrong on the server');
+                        }
+                        else if(request.status === 403)
+                        {
+                            alert('forbidden request');
+                        }
+                        else if(request.status === 404)
+                        {
+                            alert('file not found');
+                        }
+                    }
+                  };
+                  var username = document.getElementById('usr').value;
+                  var password = document.getElementById('psd').value;
+                  request.open('POST', 'http://adeepak269.imad.hasura-app.io/home/signup/register', true);
+                  request.setRequestHeader('Content-Type','application/json');
+                  request.send(JSON.stringify({"username": username, "password": password}));
                 };
