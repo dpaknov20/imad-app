fd2439f8abc21c067256770fc160efd823012f55
Deepak Agarwal <adeepak269@gmail.com>
2017-08-28 23:56:28 +0530

    Update myappmain.js

--- ui/myappmain.js
+++ ui/myappmain.js
@@ -1,1 +1,64 @@
-
+var submit=document.getElementById('submit_btn');
+                 submit.onclick = function() {
+                  //create a request
+                  var request = new XMLHttpRequest();
+                  //capture the response and store it in the variable
+                  
+                  request.onreadystatechange = function() {
+                    if(request.readyState === XMLHttpRequest.DONE)  {
+                        if(request.status === 200)
+                        {
+                            alert('logged in successfully');
+                        }
+                        else if(request.status === 500)
+                        {
+                            alert('something went wrong on the server');
+                        }
+                        else if(request.status === 403)
+                        {
+                            alert('username/password is incorrect');
+                        }
+                        else if(request.status === 404)
+                        {
+                            alert('file not found');
+                        }
+                    }
+                  };
+                  var username = document.getElementById('usr').value;
+                  var password = document.getElementById('psd').value;
+                  request.open('POST', 'http://adeepak269.imad.hasura-app.io/login', true);
+                  request.setRequestHeader('Content-Type','application/json');
+                  request.send(JSON.stringify({"username": username, "password": password}));
+            };
+                 var register=document.getElementById('register_btn');
+                 register.onclick = function() {
+                  //create a request
+                  var request = new XMLHttpRequest();
+                  //capture the response and store it in the variable
+                  
+                  request.onreadystatechange = function() {
+                    if(request.readyState === XMLHttpRequest.DONE)  {
+                        if(request.status === 200)
+                        {
+                            alert('registered successfully');
+                        }
+                        else if(request.status === 500)
+                        {
+                            alert('something went wrong on the server');
+                        }
+                        else if(request.status === 403)
+                        {
+                            alert('forbidden request');
+                        }
+                        else if(request.status === 404)
+                        {
+                            alert('file not found');
+                        }
+                    }
+                  };
+                  var username = document.getElementById('usr').value;
+                  var password = document.getElementById('psd').value;
+                  request.open('POST', 'http://adeepak269.imad.hasura-app.io/register', true);
+                  request.setRequestHeader('Content-Type','application/json');
+                  request.send(JSON.stringify({"username": username, "password": password}));
+                };
