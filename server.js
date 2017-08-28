85d269913fc765f22ebd9c6f492d8a5ad6b8f60e
Deepak Agarwal <adeepak269@gmail.com>
2017-08-29 00:56:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/counter', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'myapp.html'));
 });
 
 //function for the encryption of the passsword
