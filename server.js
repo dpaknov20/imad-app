ec2adca2343c874be0dbbe8cd92e0c2ec144239e
Deepak Agarwal <adeepak269@gmail.com>
2017-08-28 20:21:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.get('/check-login',function(req,res) {
 
 app.get('/logout',function(req,res) {
    delete req.session.outh;
-   res.send('logged out');
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
 
 app.get('/articles/:articleName', function (req, res) {
