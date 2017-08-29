6e84a6e7d567392d99e27407ec4658d85120962f
Deepak Agarwal <adeepak269@gmail.com>
2017-08-29 08:25:06 +0530

    [imad-console] Updates ui/myappmain.js

--- ui/myappmain.js
+++ ui/myappmain.js
@@ -62,7 +62,32 @@
             request.send(null);
         }
         
-        
+        function loadDetails () {
+        // Check if the user is already logged in
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (request.readyState === XMLHttpRequest.DONE) {
+                    var details = document.getElementById('details');
+                    if (request.status === 200) {
+                        var content = '<ul>';
+                        var articleData = JSON.parse(this.responseText);
+                        for (var i=0; i< articleData.length; i++) {
+                            content += `<li>
+                            <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                            (${articleData[i].date.split('T')[0]})</li>`;
+                        }
+                        content += "</ul>"
+                        details.innerHTML = content;
+                    } else {
+                        details.innerHTML('Oops! Could not load all the details!')
+                    }
+                }
+            };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
         loadLogin();
         
-                
+        loadDetails();        
