1734cefbe567d1457ce621f7a13eba1398e3f129
Deepak Agarwal <adeepak269@gmail.com>
2017-08-29 19:04:00 +0530

    [imad-console] Updates ui/myappmain.js

--- ui/myappmain.js
+++ ui/myappmain.js
@@ -73,14 +73,12 @@
             var request = new XMLHttpRequest();
             request.onreadystatechange = function () {
                 if (request.readyState === XMLHttpRequest.DONE) {
-                    if (request.status === 200) {
                         var detailData = JSON.parse(this.responseText);
                             var contentHTML = 
                             `<a href="/customer/${detailData[0].booking}">${detailData[0].name}</a>`;
                             document.getElementById('customer').innerHTML = contentHTML;
-                        }
-            }
-            };
+                    }
+                };
             request.open('GET', '/getdetails', true);
             request.send(null);
         }
