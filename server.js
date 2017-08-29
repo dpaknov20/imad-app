d6eea03dd4e9714665c068976aa471a59a5a1d50
Deepak Agarwal <adeepak269@gmail.com>
2017-08-29 17:47:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,7 @@ var Pool = require('pg').Pool;
 //for the body parsing(using JSON method)
 var bodyParser = require('body-parser');
 var session = require('express-session');
-jest.mock('react-native-i18n', () => {
-  const i18njs = require('i18n-js');
-  const en = require('../app/i18n/locales/en');
-  i18njs.translations = {en}; // Optional ('en' is the default)
 
-  return {
-     t: jest.fn((k, o) => i18njs.t(k, o)),
-  };
-});
 
 //configuraton for the database
 var config = {
