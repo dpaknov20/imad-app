c81b0f45db8c0a674f3c79cfdbf49e9f966a73c7
Deepak Agarwal <adeepak269@gmail.com>
2017-08-29 19:36:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -240,7 +240,7 @@ function makeTemplate(data) {
                 </title>
             </head>
             <body>
-                <div align="center" style="padding-top=50;>
+                <div align="center" >
                     <h2>
                     ${booking}</h2>
                     <h2>
