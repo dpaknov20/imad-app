a7bfe16f1fef3059993e55cd14826287d5e3ab95
Deepak Agarwal <adeepak269@gmail.com>
2017-08-30 23:32:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -232,13 +232,21 @@ app.get('/customer/:bookingid', function (req, res) {
             res.status(404).send('customer tagid_id not found');
         } else {
             var custData = result.rows[0];
-            res.send(makeTemplate(custData));
+            pool.query('SELECT * FROM maps WHERE city = ($1)', [custData.fromcity], function (err, result) {
+                 if (err) {
+                    res.status(500).send(err.toString());
+                }
+                else {
+                    var mapData = result.rows[0];
+                }
+            }
+            res.send(makeTemplate(custData,mapData));
         }
     }
   });
 });
 
-function makeTemplate(data) {
+function makeTemplate(data,mdata) {
     var name=data.name;
     var email=data.email;
     var contact=data.contact;
@@ -249,6 +257,7 @@ function makeTemplate(data) {
     var booking=data.booking;
     var pnr=data.pnr;
     var tagid=data.tagid;
+    var mapy=mdata.maplink;
         var bookTemplate = `
         <html>
             <head>  
@@ -260,7 +269,7 @@ function makeTemplate(data) {
                 <div align="center" style="padding-top : 5">
                 <div style="position: relative;">
                     <div style="position: absolute; top: 0; right: 0; padding-top: 10; padding-right: 10">
-                        <iframe align="right" name="demo" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3884.4246505091282!2d77.70440411437802!3d13.198634790707196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1cfe75446265%3A0x296c70e9a129418e!2sKempegowda+International+Airport+(BLR)!5e0!3m2!1sen!2sin!4v1504110165682" width="300" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        ${mapy}
                             <h4>DEPARTURE AIRPORT</h4>
                     </div>
                 </div>
