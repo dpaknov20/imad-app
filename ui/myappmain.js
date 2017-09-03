d13c39aff1d34ffc1257f107adc373889dc34853
Deepak Agarwal <adeepak269@gmail.com>
2017-09-03 18:24:20 +0530

    [imad-console] Updates ui/myappmain.js

--- ui/myappmain.js
+++ ui/myappmain.js
@@ -5,6 +5,7 @@
                     <br>
                     <input type="text" placeholder="Name" id="name" />
                     <br>
+                    <hr/>
                     <label><b>BAG TAG NUMBER</b></label>
                     <br>
                     <input type="text" placeholder="Tagid" id="tagid" />
