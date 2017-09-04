835b2157adfb5f33be004c7e73e2207bba995292
Deepak Agarwal <adeepak269@gmail.com>
2017-09-04 09:38:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -208,7 +208,6 @@ app.get('/getdetails', function (req, res) {
    });
 });
 
-
 app.get('/customer/:bookingid', function (req, res) {
   pool.query('SELECT * FROM customer WHERE tagid = ($1)', [req.session.auth.tagid], function (err, result) {
     if (err) {
