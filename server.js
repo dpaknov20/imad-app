95834101340c7b794cbdd7322d2fb027fa8eabda
Deepak Agarwal <adeepak269@gmail.com>
2017-09-12 19:44:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -225,15 +225,14 @@ app.get('/customer/:bookingid', function (req, res) {
 });
 
 app.get('/customer/baggage/:tagid', function (req, res) {
-  pool.query('SELECT * FROM customer WHERE tagid = ($1)', [req.session.auth.tagid], function (err, result) {
+  pool.query('SELECT tagid FROM customer WHERE tagid = ($1)', [req.session.auth.tagid], function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
     } else {
         if (result.rows.length === 0) {
             res.status(404).send('customer tagid_id not found');
         } else {
-            var metdata=result.rows[0];
-            res.send(metdata.tagid);
+            res.send(result.rows);
         }
     }
   });
