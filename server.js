8cdd9e989d82894e2b6a268ad261599cc71e709f
Deepak Agarwal <adeepak269@gmail.com>
2017-09-30 12:24:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get('/author/:username',function(req,res) {
       } 
       else {
             if (result.rows.length === 0) {
-                res.status(404).send('customer tagid_id not found');
+                res.status(404).send('user not found');
             } else {
                 var authordata=result.rows[0];
                 res.send(authorTemplate(auhtordata));
