eaa016f5a096321ece032671cbbfd72eb2ae5d7c
Deepak Agarwal <adeepak269@gmail.com>
2017-09-30 10:16:34 +0530

    [imad-console] Updates ui/myappmain.js

--- ui/myappmain.js
+++ ui/myappmain.js
@@ -88,3 +88,49 @@
 
         loadLogin();
         loadDetails();
+        
+        
+        
+         var register=document.getElementById('myapp_register');
+                 register.onclick = function() {
+                  //create a request
+                  var request = new XMLHttpRequest();
+                  //capture the response and store it in the variable
+                  
+                  request.onreadystatechange = function() {
+                    if(request.readyState === XMLHttpRequest.DONE)  {
+                        if(request.status === 200)
+                        {
+                            alert('registered successfully');
+                        }
+                        else if(request.status === 500)
+                        {
+                            alert('something went wrong on the server');
+                        }
+                        else if(request.status === 403)
+                        {
+                            alert('forbidden request');
+                        }
+                        else if(request.status === 404)
+                        {
+                            alert('file not found');
+                        }
+                    }
+                  };
+                  
+                  var name = document.getElementById('name').value;
+                  var email = document.getElementById('email').value;
+                  var contact = document.getElementById('contact').value;
+                  var flight = document.getElementById('flight').value;
+                  var fromcity = document.getElementById('fromcity').value;
+                  var tocity = document.getElementById('tocity').value;
+                  var date = document.getElementById('date').value;
+                  var booking = document.getElementById('book').value;
+                  var pnr = document.getElementById('pnr').value;
+                  var tagid = document.getElementById('tagid').value;
+    
+                  request.open('POST', 'http://adeepak269.imad.hasura-app.io/registration', true);
+                  request.setRequestHeader('Content-Type','application/json');
+                  request.send(JSON.stringify({"name": name, "email": email, "contact": contact, "flight": flight, "fromcity": fromcity, "tocity": tocity, "date": date, "booking": booking, "pnr": pnr, "tagid": tagid}));
+                };
+            
