e4860c1f81efbe24deb5119b1a0e5061f615c1ae
Deepak Agarwal <adeepak269@gmail.com>
2017-10-01 22:27:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,15 @@ app.get('/myfirstapp/articles',function(req,res) {
            res.status(500).send(err.toString());
        }
        else {
-           res.send(JSON.stringify(result.rows));
+           pool.query("select * from articles",function(err,result) {
+              if(err) {
+                   res.status(500).send(err.toString());
+              } 
+              else {
+                  res.send(result.rows);
+              }
+           });
+           //res.send(JSON.stringify(result.rows));
        }
    });
 });
