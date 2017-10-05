df33b6cb223cf97958e4b599877acab064f1172c
Deepak Agarwal <adeepak269@gmail.com>
2017-10-05 16:09:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -129,7 +129,7 @@ function myfirstloadLogin() {
                                 if(request.readyState === XMLHttpRequest.DONE)  {
                                     if(request.status === 200)
                                     {
-                                        var cont = `you can find your article <a href="/articles/${inp}">here</a><br/><br/>`;
+                                        var cont = `you can find your article <a href="/articles/${inp}">here</a>`;
                                         document.getElementById('milgaya').innerHTML = cont;
                                     }
                                     else
