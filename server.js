1814ba0b8d7cea1229fe19a5fa2d9b2a2dadc6f5
Deepak Agarwal <adeepak269@gmail.com>
2017-12-05 07:37:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ function hash (input,salt) {
 app.get('/hash/:input', function (req, res) {
    var hashedString = hash(req.params.input,'this-is-some-random-string');
    res.send(hashedString);
-    
 });
 
 app.post('/login',function(req,res) {
