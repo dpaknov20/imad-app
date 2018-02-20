d2d809404716cdcf82b44f6446df1fd723c09fd6
Deepak Agarwal <adeepak269@gmail.com>
2018-02-20 07:46:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/calculator/:opera/:val1/:val2', function (req, res) {
 app.post('/login',function(req,res) {
     var username = req.body.username;
     var password = req.body.password;
-    pool.query('SELECT * FROM "user_table" WHERE username = $1', [username], function(err,result) {
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err,result) {
         if(err) {
             res.status(500).send(err.toString());
         }
@@ -117,7 +117,7 @@ app.post('/register',function(req,res) {
     //var salt = crypto.randomBytes(128).toString('hex');
     //var dbstring = hash(password, salt);
     var dbstring = password;
-    pool.query('SELECT * FROM "user_table" WHERE username = $1', [username], function(err,result) {
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err,result) {
         if(err) {
             res.status(500).send(err.toString());
         }
@@ -127,7 +127,7 @@ app.post('/register',function(req,res) {
                 res.status(403).send('username already present');
             }
             else {
-                pool.query('INSERT INTO "user_table" (username, password) VALUES ($1,$2)', [username,dbstring], function(err,result) {
+                pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)', [username,dbstring], function(err,result) {
                     if(err) {
                         res.status(500).send(err.toString());
                     }
@@ -142,7 +142,7 @@ app.post('/register',function(req,res) {
 
 app.get('/check-login',function(req,res) {
    if(req.session && req.session.auth && req.session.auth.userName) {
-        pool.query('SELECT * FROM "user_table" WHERE username = $1', [req.session.auth.userName], function(err,result) {
+        pool.query('SELECT * FROM "user" WHERE username = $1', [req.session.auth.userName], function(err,result) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
