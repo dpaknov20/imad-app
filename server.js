2d0903d80c7c877a32fca50ce0642d3b838f48e2
Deepak Agarwal <adeepak269@gmail.com>
2018-03-07 19:54:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.get('/contribute',function(req,res) {
                     Title for your article: 
                     <input type="text" id="title" /><br>
                     Write the content:
-                    <textarea cols="50" rows="20" id="content">Body of the article</textarea><br>
+                    <textarea cols="5" rows="5" id="content">Body of the article</textarea><br>
                     Category of the article:
                     <input type="text" id="category" /><br>
                     <input type="submit" id="submit" onclick="contributefunc()" value="submit" />
