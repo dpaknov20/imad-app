ea977d9805417e9eeedd0cab06b2700f096a0f84
Deepak Agarwal <adeepak269@gmail.com>
2018-04-19 09:42:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ app.get('/check-login',function(req,res) {
 
 app.get('/update/channel',function(req,res) {
     if(req.session && req.session.auth && req.session.auth.userName) {
-        client.updateChannel(380103, {writeKey:'GR1IFBYTK5NKNIV6', field1: 100}, function(err, resp) {
+        client.updateChannel(380103, { writeKey:'GR1IFBYTK5NKNIV6', field1: 100}, function(err, resp) {
             if (!err && resp > 0) {
                 console.log('update successfully. Entry number was: ' + resp);
             }
