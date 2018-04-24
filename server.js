40723bed76c1425fd3abb928e3ccc8a71986107a
Deepak Agarwal <adeepak269@gmail.com>
2018-04-24 11:59:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var Pool = require('pg').Pool;
 //for the body parsing(using JSON method)
 var bodyParser = require('body-parser');
 var session = require('express-session');
-
+var ThingSpeakClient = require('thingspeakclient');
 
 //configuraton for the database
 var config = {
