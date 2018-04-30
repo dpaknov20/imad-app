58b71538711e319024a49d9afb1369a2ceba45f9
Deepak Agarwal <adeepak269@gmail.com>
2018-04-30 10:14:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.get('/counter', function (req, res) {
 
 app.get('/robot_present_values', function (req, res) {
     var point=req.body.point;  
-    alert(point);
   res.send(point.toString());
 });
 
